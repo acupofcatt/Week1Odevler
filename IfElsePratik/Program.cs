cb9ed39eb587bd41494a81593699f9b0ad41ce74
acupofcatt <115213915+acupofcatt@users.noreply.github.com>
2024-10-04 04:14:46 +0300

    Yorum satırları eklendi

--- IfElsePratik/Program.cs
+++ IfElsePratik/Program.cs
@@ -10,13 +10,13 @@ namespace Variables
         {
             Console.WriteLine("Lütfen bir sayı giriniz: ");
 
-            int.TryParse(Console.ReadLine(), out int sayi);
+            int.TryParse(Console.ReadLine(), out int sayi);     //kullanıcıdan sayı alınıyor
 
-            if (sayi == 10)
+            if (sayi == 10)     //sayının 10a göre değerlendirmesi yapılıyor
             {
                 Console.WriteLine("Girilen sayı 10'a eşittir.");
             }
-            else if (sayi <= 10)
+            else if (sayi < 10)
             {
                 Console.WriteLine("Girilen sayı 10'dan küçüktür.");
             }
@@ -26,7 +26,7 @@ namespace Variables
             }
 
 
-            if (sayi % 2 == 0)
+            if (sayi % 2 == 0)      //sayının teklik çiftlik kontrolü yapılıyor
             {
                 Console.WriteLine("Girilen sayı çifttir.");
             }
